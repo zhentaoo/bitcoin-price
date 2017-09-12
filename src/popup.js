be642601da4ab0023230bb6889fae9d02a02ca44
zhentaoo <1904974327@qq.com>
2017-09-12 17:01:32 +0800

    update icon

--- src/popup.js
+++ src/popup.js
@@ -3,15 +3,15 @@
 // found in the LICENSE file.
 
 
-// function click(e) {
-//   chrome.tabs.executeScript(null,
-//       {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-//   window.close();
-// }
-//
-// document.addEventListener('DOMContentLoaded', function () {
-//   var divs = document.querySelectorAll('div');
-//   for (var i = 0; i < divs.length; i++) {
-//     divs[i].addEventListener('click', click);
-//   }
-// });
+function click(e) {
+  chrome.tabs.executeScript(null,
+      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
