d59161d0c443d7fcd098aa3009c6f97794bb734c
zhentaoo <1904974327@qq.com>
2017-09-13 11:16:38 +0800

    找到几个稳定的接口

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-## 比特币价格，Chrome插件
+## Chrome插件--比特币、黄金、美元价格
 > 正如你看到的，这是个chrome插件，主要是显示 比特币、黄金、美元当前的RMB价格
 > 如果你对区块链、前端技术，以及投资感兴趣，那就一起参与这个项目吧！！！
+> 项目中用到的接口都是第三方的，不保证稳定性....
 
 ![./doc/bitcoin.png](./doc/bitcoin.png)
 
-## 如何使用该插件
+## 如何使用该插件?
 打开Chrome，更多工具（more tools），扩展（extensions），加载解压的扩展（load unpacked extension），选择bitcoin-price/src目录
+
+## 本项目中使用到的数据接口
+1. 虚拟币价格，聚币网：https://www.jubi.com/coin/trends
+2. 黄金价格，中金在线：http://gold.cnfol.com/fol_inc/v6.0/Gold/goldhq/json/AUT+D_new.json?lastTime=
+3. 美元汇率，雅虎金融: https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json
 
 ## 如何写一个chrome插件
 首先可以看360翻译的文档，上面有大量的demo，http://open.chrome.360.cn/extension_dev/background_pages.html
 
-## 重要API解读
-#### Browser Actions
+
+## Chrome extension--重要API解读
+### Browser Actions
 可以定义应用的图标、name、气泡html
 
-#### permissions
+### permissions
 定义该应用可以访问的域、资源
 
-#### background_scripts
+### background_scripts
 在插件启动时，就会执行该脚本，不需要HTML文件，例：background.js
 
-#### content_scripts
+### content_scripts
 在match到的页面下，会执行该脚本，例：content.js会在http://www.zhentaoo.com/*下执行
--- src/popup.js
+++ src/popup.js
@@ -8,16 +8,43 @@ document.addEventListener('DOMContentLoaded', function () {
     var eth = document.querySelector('#eth')
     var bcc = document.querySelector('#bcc')
     var gold = document.querySelector('#gold')
-    var dollar = document.querySelector('#dollar')
+    var usd = document.querySelector('#usd')
 
-    fetch('https://www.btc123.com/api/getTicker?symbol=btcchinabtccny')
+    // jubi 网 api
+    fetch('https://www.jubi.com/coin/trends')
       .then(res => res.json())
       .then(res => {
-        btc.innerHTML = res.datas.ticker.buy
+        btc.innerHTML = res.btc.yprice.toFixed(2)
+        bcc.innerHTML = res.bcc.yprice.toFixed(2)
+        eth.innerHTML = res.eth.yprice.toFixed(2)
       })
       .catch(res => {
-        btc.innerHTML = res
+        btc.innerHTML = '--'
       })
+
+    // 雅虎 金融接口，美元汇率
+    fetch('https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json')
+      .then(res => res.json())
+      .then(res => {
+        var item = res.list.resources.find(el => el.resource.fields.name == 'USD/CNY')
+        usd.innerHTML = item.resource.fields.price
+      })
+      .catch(res => {
+        usd.innerHTML = '--'
+      })
+
+    // 黄金
+    fetch('http://gold.cnfol.com/fol_inc/v6.0/Gold/goldhq/json/AUT+D_new.json?lastTime=' + new Date().getTime())
+      .then(res => res.json())
+      .then(res => {
+        // alert(res.result.last_price)
+        gold.innerHTML = res[1][1].toFixed(2)+"/g"
+        // gold.innerHTML = 999
+      })
+      .catch(res => {
+        gold.innerHTML = '--/g'
+      })
+
   }
 
   getAllPriceInfo();
