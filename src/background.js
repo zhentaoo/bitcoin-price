7081c3be65fb00cb437836d03cbd6b542c478617
zhentaoo <1904974327@qq.com>
2018-11-12 14:56:37 +0800

    rm -rf ad

--- src/background.js
+++ src/background.js
@@ -2,8 +2,7 @@
 chrome.tabs.onActivated.addListener(function (tab) {
     chrome.tabs.executeScript(null, {code: `
       function clearAD() {
-        console.log(222)
-        document.querySelectorAll('.slides').forEach(el => el.style.display = 'none')
+        document.querySelectorAll('.ads').forEach(el => el.style.display = 'none')
       }
       clearAD()
 
